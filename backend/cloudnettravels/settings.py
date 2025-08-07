d4fb581e3254d9b7be95a4e1e6fd405f0136f6b9
Midhun Vijay <midhunvijayvs@gmail.comgit config --global user.email midhunvijayvs@gmail.comgit config --global user.email midhunvijayvs@gmail.com>
2025-08-07 15:37:14 +0530

    conf

--- backend/cloudnettravels/settings.py
+++ backend/cloudnettravels/settings.py
@@ -70,13 +70,38 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # React development server
-    "https://midhunvijayvs.github.io",  # dev React domain
-    "https://cloudnettravels.com",
-    "https://booking.cloudnettravels.com", # Production React domain
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",  # React development server
+#     "https://midhunvijayvs.github.io",  # dev React domain
+#     "https://cloudnettravels.com",
+#     "https://booking.cloudnettravels.com", # Production React domain
+# ]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
 ]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 #setting the path to the root urls file
 ROOT_URLCONF = 'cloudnettravels.urls'
