da9280c35aeafd83e9f5a291a9c9c81e926f551e
Midhun Vijay <midhunvijayvs@gmail.comgit config --global user.email midhunvijayvs@gmail.comgit config --global user.email midhunvijayvs@gmail.com>
2025-09-08 11:58:18 +0530

    backend url setup

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
-const BACKEND_URL='http://127.0.0.1:8000/';
-// const BACKEND_URL='https://booking.cloudnettravels.com/';
+
+var BACKEND_URL='https://booking.cloudnettravels.com/';
+
+if (process.env.REACT_APP_ENVIRONMENT=="local"){
+     BACKEND_URL='http://127.0.0.1:8000/';
+}
+
 const instance = axios.create({
   baseURL: BACKEND_URL,
   headers: {
