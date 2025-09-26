b184f0f13e79d2582be75d8e3a30bb3fccee73a1
Midhun Vijay <midhunvijayvs@gmail.comgit config --global user.email midhunvijayvs@gmail.comgit config --global user.email midhunvijayvs@gmail.com>
2025-09-26 14:27:07 +0530

    bug

--- backend/omairiq_proxy/views.py
+++ backend/omairiq_proxy/views.py
@@ -24,6 +24,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from users.customPermissions import IsAgencyUser
 from rest_framework import status
+from decimal import Decimal
 
 
 
